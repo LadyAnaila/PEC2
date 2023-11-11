049ad680b707bdd4dd6dba4d1b11f4cb3238ae4b
Noelia López Galiana <124814658+LadyAnaila@users.noreply.github.com>
2023-11-11 18:50:41 +0100

    Entrega definitiva ejercicio 2

---
+++ PEC2/PEC2_Ej2/Ejer2-2-expense-tracker/models/expense.model.js
@@ -0,0 +1,25 @@
+/**
+ * @class Model
+ *
+ * Manages the data of the application.
+ */
+
+
+class ExpenseModel {
+  constructor(text, amount){
+      this.id = this._setId();
+      this.text = text;
+      this.amount = amount;
+  }
+
+  _setId(){
+      return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+          (
+              c ^
+              (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+          ).toString(16)
+      );
+  }
+}
+
+export default ExpenseModel;
